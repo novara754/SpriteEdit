aea395f6d14fa0adda022036ffe30f3e2e544ddd
vzwGrey <vzwgrey@gmail.com>
2021-05-25 15:02:20 +0200

    fix: disable warning for nameless structs in glm

--- SpriteEdit/common.h
+++ SpriteEdit/common.h
@@ -6,10 +6,13 @@
 #include <string_view>
 
 // Math
+#pragma warning(push)
+#pragma warning(disable: 4201) // Disable warnings about nameless structs
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#pragma warning(pop)
 
 // UI
 #include <imgui.h>
